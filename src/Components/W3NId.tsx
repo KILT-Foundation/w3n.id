2c1afcf1ba7803995df3583fd7e370554828b35a
usmangeek <usman@galaniprojects.de>
2022-03-20 08:43:58 +0100

    fix: refractored code

--- src/Components/W3NId.tsx
+++ src/Components/W3NId.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Footer } from './Footer'
 import styled from 'styled-components'
+import { SearchComponent } from './SearchComponent'
 
 const StyledBody = styled.div`
   height: 100vh;
@@ -10,50 +11,11 @@ const StyledBody = styled.div`
   flex-direction: column;
   overflow-y: hidden;
 `
-const StyledSearch = styled.div`
-  display: flex;
-  width: 766px;
-  height: 40px;
-  border: solid;
-  border-color: black;
-  border-radius: 30px;
-  margin: auto;
-  padding-left: 10px;
-  margin-top: 200px;
-`
-const StyledSearchBtn = styled.button`
-  width: 160px;
-  height: 30px;
-  background-color: black;
-  color: white;
-  border-radius: 30px;
-`
-const StyledInput = styled.input`
-  width: 400px;
-  color: black;
-  background: transparent;
-  border: none;
-`
-const StyledSearchBtnWrapper = styled.div`
-  width: 180px;
-  height: 40px;
-  margin-left: auto;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`
 
 export const W3NId = () => {
   return (
     <StyledBody>
-      <StyledSearch>
-        <StyledInput type="text" placeholder="Type DID or W3Name" />
-
-        <StyledSearchBtnWrapper>
-          <StyledSearchBtn>Search</StyledSearchBtn>
-        </StyledSearchBtnWrapper>
-      </StyledSearch>
-
+      <SearchComponent />
       <Footer />
     </StyledBody>
   )
