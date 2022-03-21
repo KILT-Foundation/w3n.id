bf91be3e31520dcbdebc148a9c5e4343f9f9e910
usmangeek <usman@galaniprojects.de>
2022-03-21 13:11:25 +0100

    feat: added new footer links

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -10,28 +10,55 @@ const StyledFooter = styled.div`
   justify-content: center;
   align-items: center;
 `
-const StyledFooterLinksWrapper = styled.div`
+const StyledFooterLinksContainer = styled.div`
   display: flex;
   flex-direction: row;
+  width: 90%;
+  max-width: 766px;
   align-items: center;
   justify-content: center;
   color: white;
   font-size: 14px;
   line-height: 16px;
   letter-spacing: 0.1px;
-  gap: 2px;
+  text-align: left;
+`
+const ImprintContainer = styled.div`
+  width: 63px;
+  display: flex;
+  justify-content: flex-start;
+`
+const LinksContainer = styled.div`
+  width: 80%;
+  max-width: 600px;
+  display: flex;
+  justify-content: center;
+  gap: 4px;
+`
+const LogoContainer = styled.div`
+  width: 103px;
+  display: flex;
+  justify-content: flex-end;
 `
 export const Footer = () => {
   return (
     <StyledFooter>
-      <StyledFooterLinksWrapper>
-        <span>Imprint</span>
-        <span>-</span>
-        <span>Terms and Conditions</span>
-        <span>-</span>
+      <StyledFooterLinksContainer>
+        <ImprintContainer>
+          <span>Imprint</span>
+        </ImprintContainer>
+        <LinksContainer>
+          <span>Terms and Conditions</span>
+          <span>-</span>
 
-        <span>Privacy Policy</span>
-      </StyledFooterLinksWrapper>
+          <span>Privacy Policy</span>
+          <span>-</span>
+          <span>Tech Support</span>
+        </LinksContainer>
+        <LogoContainer>
+          <span>Built on KILT</span>
+        </LogoContainer>
+      </StyledFooterLinksContainer>
     </StyledFooter>
   )
 }
