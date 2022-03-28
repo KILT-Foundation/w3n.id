86e3fc274a8cb26a247107de2e3be9b92b066541
usmangeek <usman@galaniprojects.de>
2022-03-28 13:42:39 +0200

    fix: pink to green light theme color

--- src/Themes/Theme.ts
+++ src/Themes/Theme.ts
@@ -1,33 +1,34 @@
-const LightTheme = {
-  web3name: '#F72585',
-  btnborder: '#F72585',
+export const LightTheme = {
+  web3name: '#2D8B00',
+  btnborder: '#2D8B00',
   searchbackground: '#EAEAEA',
   searchbtn: '#DF8601',
-  seperator: '#6667ab',
-  headersecondary: 'rgba(247,37,133,0.5)',
-  fetchbackground: 'rgba(247, 37, 133, 0.15)',
+  seperator: '#2D8B00',
+  headersecondary: 'rgb(45, 139, 0)',
+  fetchbackground: 'rgb(45, 139, 0,0.15)',
   headerbottom: '#d8d8d8',
   searchbar: '#d8d8d8',
   text: '#0d0d0d',
   searchbtntext: '#F3F3F3',
   headertext: '#f3f3f3',
   body: '#fff',
+  taketour: '#2D9403',
 }
 const DarkTheme = {
   body: '#000',
-
   web3name: '#47E000',
   btnborder: '#3DC000',
   searchbar: '#d8d8d8',
   searchbackground: '#191919',
   searchbtn: '#DF8601',
   seperator: '#3DC000',
-  headersecondary: 'rgba(61,192,0,0.5)',
+  headersecondary: '#256606',
   fetchbackground: 'rgba(61,192,0,0.15)',
   headerbottom: '#454545',
   text: '#f3f3f3',
   searchbtntext: '#F3F3F3',
   headertext: '#f3f3f3',
+  taketour: '#2D7011',
 }
 export const Theme: any = {
   light: LightTheme,
