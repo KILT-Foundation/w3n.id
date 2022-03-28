4e9401e45af6bd714372512263d67b901eb4f77f
usmangeek <usman@galaniprojects.de>
2022-03-28 13:39:38 +0200

    feat: added fetch credential

---
+++ src/Components/Credentials.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import styled from 'styled-components'
+
+interface IDIDCredential {
+  credential: any
+  attesterDid: string
+  isCredentialValid: boolean
+}
+
+const Container = styled.div`
+  display: flex;
+  justify-content: start;
+  width: 100%;
+  gap: 20px;
+`
+const CredentialContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  flex-wrap: wrap;
+  width: 100%;
+  gap: 10px;
+  margin-top: 20px;
+`
+const CredentialSpan = styled.span`
+  display: block;
+  font-family: 'Overpass';
+  font-size: 14px;
+  letter-spacing: 0.1px;
+  line-height: 22px;
+  width: 80%;
+  max-width: 400px;
+  word-break: break-all;
+`
+const CredentialTitle = styled.span`
+  max-width: 100px;
+  width: 20%;
+  font-family: 'Overpass';
+  font-size: 14px;
+  letter-spacing: 0.1px;
+  line-height: 22px;
+  text-align: left;
+`
+export const Credentials = (didCredential: IDIDCredential) => {
+  return (
+    <CredentialContainer>
+      {Object.keys(didCredential.credential).map((key, index) => (
+        <Container key={index}>
+          <CredentialTitle>{key}</CredentialTitle>
+          <CredentialSpan>{didCredential.credential[key]}</CredentialSpan>
+        </Container>
+      ))}
+      <Container>
+        <CredentialTitle>Attester Did</CredentialTitle>
+        <CredentialSpan>{didCredential.attesterDid}</CredentialSpan>
+      </Container>
+    </CredentialContainer>
+  )
+}
--- src/Components/DidDocument.tsx
+++ src/Components/DidDocument.tsx
@@ -1,15 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import { ReactComponent as Copy } from '../ImageAssets/copy2clipboard_light.svg'
+import { Credentials } from './Credentials'
+import { Credential, RequestForAttestation, Did } from '@kiltprotocol/sdk-js'
+import {
+  getAttestationForRequest,
+  getDidForAccount,
+  validateAttestation,
+  validateCredential,
+} from '../Utils/search-helpers'
+import { ReactComponent as Open } from '../ImageAssets/chevron_down_white.svg'
 
-interface Endpoint {
+interface IEndpoint {
   endpointType: string
   endpointURL: string
+  did: string
 }
-
+interface Style {
+  rotate: string
+}
 const EndpointContainer = styled.div`
   display: flex;
-  justify-content: end;
+  justify-content: start;
+  flex-wrap: wrap;
   width: 100%;
 `
 const EndpointTypeSpan = styled.span`
@@ -26,12 +39,12 @@ const FetchBtn = styled.button`
   cursor: pointer;
   font-family: 'Overpass';
   text-transform: uppercase;
+  position: relative;
   color: ${(props) => props.theme.web3name};
   font-size: 14px;
   letter-spacing: 0.1px;
   line-height: 22px;
   text-align: center;
-  margin-left: auto;
   text-align: center;
   align-items: center;
   justify-content: center;
@@ -47,7 +60,8 @@ const EndpointURLSpan = styled.span`
   font-size: 14px;
   letter-spacing: 0.1px;
   line-height: 22px;
-  width: 400px;
+  width: 90%;
+  max-width: 400px;
   text-overflow: ellipsis;
   overflow: hidden;
   white-space: nowrap;
@@ -55,10 +69,10 @@ const EndpointURLSpan = styled.span`
 const UrlContainer = styled.div`
   display: flex;
   justify-content: flex-start;
-  flex-wrap: wrap;
   align-items: center;
   gap: 5px;
-  width: 70%;
+  width: 95%;
+  max-width: 500px;
 `
 
 const Container = styled.div`
@@ -75,8 +89,65 @@ const CopySvg = styled(Copy)`
   fill: ${(props) => props.theme.btnborder};
   cursor: pointer;
 `
+const OpenSvg = styled(Open)`
+  fill: ${(props) => props.theme.btnborder};
+  cursor: pointer;
+  position: absolute;
+  right: 12px;
+  top: 6px;
+  transform: rotate(${(props: Style) => props.rotate});
+`
 
-export const DidDocument = (props: Endpoint) => {
+export const DidDocument = (props: IEndpoint) => {
+  const [credential, setCredential] = useState<any | null>(null)
+  const [isCredentialValid, setIsCredentialValid] = useState<boolean>(true)
+  const [attester, setAttester] = useState<string>('')
+  const [error, setError] = useState<string | null>(null)
+  const [fetching, setFetching] = useState<boolean>(false)
+  const [fetched, setFetched] = useState<boolean>(false)
+
+  const handleFetch = () => {
+    if (fetched) {
+      setFetched(false)
+      setCredential(null)
+      setError(null)
+
+      return
+    } else {
+      setFetched(true)
+    }
+    if (credential) {
+      return
+    }
+    setFetching(true)
+    fetch(props.endpointURL)
+      .then((response) => response.json())
+      .then(async (result) => {
+        if (!Did.DidUtils.isSameSubject(result.claim.owner, props.did)) {
+          setIsCredentialValid(false)
+          setAttester('Credential subject and signer DID are not the same')
+        } else if (Credential.isICredential(result)) {
+          setIsCredentialValid(await validateCredential(result))
+          setAttester(getDidForAccount(result.attestation.owner))
+        } else if (RequestForAttestation.isIRequestForAttestation(result)) {
+          const attestation = await getAttestationForRequest(result)
+          setIsCredentialValid(await validateAttestation(attestation))
+          if (attestation) {
+            setAttester(getDidForAccount(attestation.owner))
+          } else {
+            setAttester('No Attestation found')
+          }
+        }
+
+        setCredential(result.claim.contents)
+        setFetching(false)
+      })
+      .catch((error) => {
+        console.log(error)
+        setFetched(false)
+        setFetching(false)
+      })
+  }
   return (
     <Container>
       <EndpointTypeSpan>{props.endpointType}</EndpointTypeSpan>
@@ -90,8 +161,18 @@ export const DidDocument = (props: Endpoint) => {
             }}
           />
         </UrlContainer>
-        <FetchBtn>Fetch</FetchBtn>
+        <FetchBtn onClick={() => handleFetch()}>
+          Fetch
+          <OpenSvg rotate={fetched ? '180deg' : '0deg'} />
+        </FetchBtn>
       </EndpointContainer>
+      {credential && (
+        <Credentials
+          credential={credential}
+          attesterDid={attester}
+          isCredentialValid={isCredentialValid}
+        />
+      )}
       <Seperator />
     </Container>
   )
