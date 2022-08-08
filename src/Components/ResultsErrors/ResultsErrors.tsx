6e3ef66b26205971bda7ca65769a19e8ef545480
Luke Grunau <lukegrunau@lukes-mbp.speedport.ip>
2022-08-08 10:45:48 +0200

    more error changes

--- src/Components/ResultsErrors/ResultsErrors.tsx
+++ src/Components/ResultsErrors/ResultsErrors.tsx
@@ -1,26 +1,5 @@
 import styles from './ResultsErrors.module.css';
 
-export type Error =
-  | 'not_claimed'
-  | 'max_limit'
-  | 'invalid_chars'
-  | 'min_limit'
-  | 'invalid_kilt'
-  | 'no_linked_account';
-
-const errorMessages = {
-  max_limit: 'Maximum 30 characters allowed',
-  min_limit: 'Minimum characters length should be 3',
-  invalid_chars: 'Invalid Characters',
-  invalid_kilt: 'Not a valid Kilt DID',
-  no_linked_account: 'No web3name has been linked to this account',
-};
-
-interface Props {
-  name: string;
-  error: Error;
-}
-
 const ClaimWeb3Name = ({ name }: { name: string }) => {
   return (
     <div>
@@ -93,30 +72,42 @@ const ClaimWeb3Name = ({ name }: { name: string }) => {
   );
 };
 
-const ErrorMessage = ({
-  error,
-}: {
-  error:
-    | 'max_limit'
-    | 'invalid_chars'
-    | 'min_limit'
-    | 'invalid_kilt'
-    | 'no_linked_account';
-}) => {
+export type Error =
+  | 'not_claimed'
+  | 'max_limit'
+  | 'invalid_chars'
+  | 'min_limit'
+  | 'invalid_kilt'
+  | 'no_linked_account';
+
+const errorMessages = {
+  max_limit: 'Maximum 30 characters allowed',
+  min_limit: 'Minimum characters length should be 3',
+  invalid_chars: 'Invalid Characters',
+  invalid_kilt: 'Not a valid Kilt DID',
+  no_linked_account: 'No web3name has been linked to this account',
+};
+
+const ErrorMessage = ({ message }: { message: string }) => {
   return (
     <div className={styles.container}>
       <span className={styles.note}>Error</span>
       <div className={styles.claimW3NSteps}>
-        <span className={styles.step}>{errorMessages[error]}</span>
+        <span className={styles.step}>{message}</span>
       </div>
     </div>
   );
 };
 
+interface Props {
+  name: string;
+  error: Error;
+}
+
 export const ResultsErrors = ({ name, error }: Props) => {
   return error === 'not_claimed' ? (
     <ClaimWeb3Name name={name} />
   ) : (
-    <ErrorMessage error={error} />
+    <ErrorMessage message={errorMessages[error]} />
   );
 };
