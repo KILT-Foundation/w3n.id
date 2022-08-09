19d4421638525f1c518fd472f5f0993b28cbfea4
Muhammad Usman <94175349+usmangeek@users.noreply.github.com>
2022-08-09 17:12:19 +0200

    SK-1066: footer and legal changes (#29)
    * SK-1066: footer and legal changes
    
    * fix: remove custom scrollbar
    
    * fix: remove privacy heading

--- src/Components/Footer/Footer.module.css.d.ts
+++ src/Components/Footer/Footer.module.css.d.ts
@@ -1,12 +1,9 @@
 declare const styles: {
   readonly footer: string;
   readonly content: string;
-  readonly anchor: string;
-  readonly anchorActive: string;
-  readonly imprint: string;
-  readonly imprintHeading: string;
-  readonly imprintLine: string;
-  readonly spacedLine: string;
-  readonly kiltLogo: string;
+  readonly navMenu: string;
+  readonly navLink: string;
+  readonly navLinkActive: string;
+  readonly copyright: string;
 };
 export = styles;
