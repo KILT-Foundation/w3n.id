b15307d528f5e3c549b084fce3cdc84fe2ade44f
Tom Adler <me@arty.name>
2022-11-09 16:24:26 +0100

    don’t connect twice

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,10 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { StrictMode } from 'react';
-import { connect } from '@kiltprotocol/sdk-js';
 
 import './index.css';
 
 import { App } from './Components/App/App';
-
-connect(process.env.REACT_APP_CHAIN_ENDPOINT as string);
 
 const container = document.getElementById('root') as HTMLElement;
 
