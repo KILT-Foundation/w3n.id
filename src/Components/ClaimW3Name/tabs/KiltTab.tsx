13debd1b34c9abe311ad80f12a9ba9db8a01a08d
Tom Adler <me@arty.name>
2023-02-10 10:07:30 +0100

    SK-1671: show balances (#142)

--- src/Components/ClaimW3Name/tabs/KiltTab.tsx
+++ src/Components/ClaimW3Name/tabs/KiltTab.tsx
@@ -1,12 +1,11 @@
-import { FormEvent, useCallback, useState } from 'react';
-
-import { ChainHelpers } from '@kiltprotocol/sdk-js';
-
+import { FormEvent, Fragment, useCallback, useRef, useState } from 'react';
+import { ChainHelpers, BalanceUtils } from '@kiltprotocol/sdk-js';
 import { web3FromSource } from '@polkadot/extension-dapp';
 
 import styles from '../ClaimW3Name.module.css';
 
 import {
+  apiPromise,
   getAccounts,
   getW3NameExtrinsic,
   InjectedAccount,
@@ -21,6 +20,13 @@ export function KiltTab({ web3name }: Props) {
   const [filteredAccounts, setFilteredAccounts] = useState<InjectedAccount[]>(
     [],
   );
+  const [unusableAccounts, setUnusableAccounts] = useState<InjectedAccount[]>(
+    [],
+  );
+  const balances = useRef(
+    new Map<string, typeof BalanceUtils.KILT_COIN>(),
+  ).current;
+
   const [selectedAccount, setSelectedAccount] = useState<InjectedAccount>();
   const [connecting, setConnecting] = useState(false);
   const [showOptions, setShowOptions] = useState(false);
@@ -32,12 +38,37 @@ export function KiltTab({ web3name }: Props) {
   const handleConnectWallets = useCallback(async () => {
     try {
       setConnecting(true);
+
+      const api = await apiPromise;
       const accounts = await getAccounts();
-      setFilteredAccounts(accounts);
+
+      for (const { address } of accounts) {
+        if (balances.has(address)) {
+          continue;
+        }
+        const result = await api.query.system.account(address);
+        const { free, miscFrozen } = result.data;
+        const transferable = free.sub(miscFrozen);
+        balances.set(address, transferable);
+      }
+
+      const deposit = api.consts.web3Names.deposit;
+      const fee = BalanceUtils.toFemtoKilt('0.0045'); // TODO: update when it changes
+      const total = deposit.add(fee);
+
+      const usable = accounts.filter(({ address }) =>
+        balances.get(address)?.gte(total),
+      );
+      const unusable = accounts.filter(({ address }) =>
+        balances.get(address)?.lt(total),
+      );
+
+      setFilteredAccounts(usable);
+      setUnusableAccounts(unusable);
     } finally {
       setConnecting(false);
     }
-  }, []);
+  }, [balances]);
 
   const handleSubmit = useCallback(
     async (event: FormEvent<HTMLFormElement>) => {
@@ -123,6 +154,38 @@ export function KiltTab({ web3name }: Props) {
                       </span>
                     </li>
                   ))}
+                  {unusableAccounts.length > 0 && (
+                    <Fragment>
+                      <li
+                        className={styles.option}
+                        aria-disabled="true"
+                        onClick={(event) => event.stopPropagation()}
+                      >
+                        Insufficient transferable balance:
+                      </li>
+                      {unusableAccounts.map((account) => {
+                        const balance = balances.get(account.address);
+                        if (!balance) {
+                          return null;
+                        }
+                        const formatted =
+                          BalanceUtils.formatKiltBalance(balance);
+                        const full = formatted === '0' ? '0 KILT' : formatted;
+                        return (
+                          <li
+                            className={styles.option}
+                            aria-disabled="true"
+                            onClick={(event) => event.stopPropagation()}
+                            key={account.address}
+                          >
+                            <span className={styles.optionText}>
+                              [{full}] {account.meta.name}
+                            </span>
+                          </li>
+                        );
+                      })}
+                    </Fragment>
+                  )}
                 </ul>
               )}
             </div>
