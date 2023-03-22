d2a730b4f1ba1980a96da14d2eded6327dd422be
Luke Grunau <lukegrunau@gmail.com>
2023-03-22 16:24:21 +0100

    Credential label (#190)

--- src/Components/CredentialErrors/CredentialErrors.module.css.d.ts
+++ src/Components/CredentialErrors/CredentialErrors.module.css.d.ts
@@ -1,8 +1,8 @@
 declare const styles: {
   readonly definitions: string;
-  readonly container: string;
-  readonly credentialTitle: string;
-  readonly credentialDescription: string;
+  readonly definition: string;
+  readonly title: string;
+  readonly description: string;
   readonly invalid: string;
 };
 export = styles;
