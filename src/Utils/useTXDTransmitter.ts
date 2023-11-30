105d8b7f5668da082789bb7a01d10d353abf211c
lukeg90 <lukegrunau@gmail.com>
2023-11-30 15:39:50 +0100

    Add OnFinality endpoint

--- src/Utils/useTXDTransmitter.ts
+++ src/Utils/useTXDTransmitter.ts
@@ -8,6 +8,8 @@ import { endpoint } from './claimWeb3name-helpers';
 const txdUrls: Record<string, string> = {
   'wss://kilt-rpc.dwellir.com': 'https://txd.trusted-entity.io',
   'wss://spiritnet.kilt.io': 'https://txd.trusted-entity.io',
+  'wss://spiritnet.api.onfinality.io/public-ws':
+    'https://txd.trusted-entity.io',
   'wss://peregrine.kilt.io': 'https://dev.txd.trusted-entity.io',
   'wss://peregrine-stg.kilt.io/para': 'https://smoke.txd.trusted-entity.io',
 };
@@ -15,6 +17,7 @@ const txdUrls: Record<string, string> = {
 const checkoutUrls: Record<string, string> = {
   'wss://kilt-rpc.dwellir.com': 'https://checkout.kilt.io',
   'wss://spiritnet.kilt.io': 'https://checkout.kilt.io',
+  'wss://spiritnet.api.onfinality.io/public-ws': 'https://checkout.kilt.io',
   'wss://peregrine.kilt.io': 'https://dev.checkout.kilt.io',
   'wss://peregrine-stg.kilt.io/para': 'https://smoke.checkout.kilt.io',
 };
