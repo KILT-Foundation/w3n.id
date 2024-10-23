82801b1ef0ca2e1335cdac5c7fa93b6d961e5eb7
Andrés <105802444+kilted-andres@users.noreply.github.com>
2024-10-23 14:25:14 +0200

    feat: envis for txd and checkout-service URLs (#580)
    New environment variables:
    
    REACT_APP_CHECKOUT_URL
    REACT_APP_TXD_URL

--- src/Utils/useTXDTransmitter.ts
+++ src/Utils/useTXDTransmitter.ts
@@ -3,24 +3,15 @@ import ky from 'ky';
 
 import { type KiltAddress } from '@kiltprotocol/sdk-js';
 
-import { endpoint } from './claimWeb3name-helpers';
+export const checkoutServiceURL = process.env.REACT_APP_CHECKOUT_URL as string;
+if (!checkoutServiceURL) {
+  throw new Error('No URL for the Checkout Service provided.');
+}
 
-const txdUrls: Record<string, string> = {
-  'wss://kilt-rpc.dwellir.com': 'https://txd.trusted-entity.io',
-  'wss://spiritnet.kilt.io': 'https://txd.trusted-entity.io',
-  'wss://spiritnet.api.onfinality.io/public-ws':
-    'https://txd.trusted-entity.io',
-  'wss://peregrine.kilt.io': 'https://dev.txd.trusted-entity.io',
-  'wss://peregrine-stg.kilt.io/para': 'https://smoke.txd.trusted-entity.io',
-};
-
-const checkoutUrls: Record<string, string> = {
-  'wss://kilt-rpc.dwellir.com': 'https://checkout.kilt.io',
-  'wss://spiritnet.kilt.io': 'https://checkout.kilt.io',
-  'wss://spiritnet.api.onfinality.io/public-ws': 'https://checkout.kilt.io',
-  'wss://peregrine.kilt.io': 'https://dev.checkout.kilt.io',
-  'wss://peregrine-stg.kilt.io/para': 'https://smoke.checkout.kilt.io',
-};
+const txdUrl = process.env.REACT_APP_TXD_URL as string;
+if (!txdUrl) {
+  throw new Error('No URL for the Transaction Daemon provided.');
+}
 
 function useApi<Output>(key: Key) {
   return useSWR<Output, string | Error>(
@@ -29,15 +20,12 @@ function useApi<Output>(key: Key) {
   );
 }
 
-export const checkoutServiceURL = checkoutUrls[endpoint];
-
-export function useApiTXDAddress() {
-  const txdUrl = txdUrls[endpoint];
-  return useApi<{ paymentAddress: KiltAddress }>(`${txdUrl}/meta`);
-}
-
 export function useApiTXDCosts() {
   return useApi<{ did: string; w3n: string }>(
     `${checkoutServiceURL}/api/costs`,
   );
+}
+
+export function useApiTXDAddress() {
+  return useApi<{ paymentAddress: KiltAddress }>(`${txdUrl}/meta`);
 }
