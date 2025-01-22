0d1750cd62042e538dadfa1c91e6064c3821ceeb
Raphael Flechtner <raphael@kilt.io>
2025-01-22 16:49:18 +0100

    chore: fix linter error

--- src/Components/VerificationMethod/VerificationMethod.tsx
+++ src/Components/VerificationMethod/VerificationMethod.tsx
@@ -9,24 +9,30 @@ interface Props {
 export const uniresolverEndpoint = process.env
   .REACT_APP_UNIRESOLVER_ENDPOINT as string;
 
-export const VerificationMethod = (props: Props) => {
+export const VerificationMethod = ({ did }: Props) => {
   return (
     <div className={styles.container}>
       <span className={styles.title}>Verification methods</span>
 
-      <div className={styles.wrapper}>
-        <span className={styles.text}>
-          Find out more about verification methods and keys here:
-        </span>
-        <a
-          className={styles.anchor}
-          href={`${uniresolverEndpoint}/#${props.did}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          {`${uniresolverEndpoint}/#\n${props.did}`}
-        </a>
-      </div>
+      {did ? (
+        <div className={styles.wrapper}>
+          <span className={styles.text}>
+            Find out more about verification methods and keys here:
+          </span>
+          <a
+            className={styles.anchor}
+            href={`${uniresolverEndpoint}/#${did}`}
+            target="_blank"
+            rel="noreferrer"
+          >
+            {`${uniresolverEndpoint}/#\n${did}`}
+          </a>
+        </div>
+      ) : (
+        <div className={styles.wrapper}>
+          <span className={styles.text}>-</span>
+        </div>
+      )}
     </div>
   );
 };
